fa08cf6bc394ef0d4ff2792269815d08fcafd013
Nikolas Smith <nikolassmith21@gmail.com>
2019-02-06 23:39:29 -0500

    Updates

---
+++ precache-manifest.6f5e78454c6a6fcf1484a8f1ce6e28a1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robotfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "036993615acbccafb03f",
+    "url": "/robotfriends/static/js/runtime~main.03699361.js"
+  },
+  {
+    "revision": "90b833745f5479593fef",
+    "url": "/robotfriends/static/js/main.90b83374.chunk.js"
+  },
+  {
+    "revision": "19dce6e4010918f21a1a",
+    "url": "/robotfriends/static/js/1.19dce6e4.chunk.js"
+  },
+  {
+    "revision": "90b833745f5479593fef",
+    "url": "/robotfriends/static/css/main.43855b1e.chunk.css"
+  },
+  {
+    "revision": "19dce6e4010918f21a1a",
+    "url": "/robotfriends/static/css/1.7d689b6b.chunk.css"
+  },
+  {
+    "revision": "f2bd9ca7410a6fea526be2b34ea9e7b9",
+    "url": "/robotfriends/index.html"
+  }
+];
